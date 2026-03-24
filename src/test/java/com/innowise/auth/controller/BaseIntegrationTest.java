22957599bfb1e0071c160bbeb6c3bc0c1acbfc40
Nikita <boreyko1901@mail.ru>
2026-03-25 00:37:28 +0300

    feature: create full AuthService

--- src/test/java/com/innowise/auth/controller/BaseIntegrationTest.java
+++ src/test/java/com/innowise/auth/controller/BaseIntegrationTest.java
@@ -2,10 +2,8 @@ package com.innowise.auth.controller;
 
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
